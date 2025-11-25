afc67a06775946e010ee7728300f762bea2466fe
AVØIID <huberjonathan@outlook.de>
2025-11-25 21:44:21 +0100

    format change

--- SoundIntervalCollection.cs
+++ SoundIntervalCollection.cs
@@ -19,41 +19,41 @@ namespace Escape_Room
                 E_Sound.Movement,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound() { frequency = 523, duration = 80 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.WallCollision,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound() { frequency = 330, duration = 80 },
                         new Sound() { frequency = 294, duration = 80 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.KeyPickup,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound() { frequency = 392, duration = 100 },
                         new Sound() { frequency = 523, duration = 100 },
                         new Sound() { frequency = 659, duration = 100 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.LevelFinished,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound { frequency = 196, duration = 100 },
                         new Sound { frequency = 440, duration = 80 },
                         new Sound { frequency = 220, duration = 100 },
@@ -61,15 +61,15 @@ namespace Escape_Room
                         new Sound { frequency = 196, duration = 100 },
                         new Sound { frequency = 392, duration = 80 },
                         new Sound { frequency = 196, duration = 120 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.GameFinished,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound { frequency = 523, duration = 250 },
                         new Sound { frequency = 659, duration = 250 },
                         new Sound { frequency = 784, duration = 250 },
@@ -77,42 +77,42 @@ namespace Escape_Room
                         new Sound { frequency = 784, duration = 250 },
                         new Sound { frequency = 659, duration = 250 },
                         new Sound { frequency = 523, duration = 250 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.InvalidInput,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound { frequency = 164, duration = 60 },
                         new Sound { frequency = 147, duration = 60 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.ValidInput,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound { frequency = 220, duration = 50 },
                         new Sound { frequency = 261, duration = 50 }
-                    }
+                    ]
                 }
             },
             {
                 E_Sound.EndScreen,
                 new SoundInterval()
                 {
-                    sounds = new Sound[]
-                    {
+                    sounds =
+                    [
                         new Sound { frequency = 100, duration = 50 },
                         new Sound { frequency = 500, duration = 50 },
                         new Sound { frequency = 100, duration = 50 },
                         new Sound { frequency = 500, duration = 50 }
-                    }
+                    ]
                 }
             }
         };
