6fe5766763a900c7d00e36e746c3ecadc201a21b
AVØIID <huberjonathan@outlook.de>
2025-11-25 21:34:09 +0100

    quick fixes to ensure compatability :)

--- SoundIntervalCollection.cs
+++ SoundIntervalCollection.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Escape_Room
 {
@@ -15,103 +12,107 @@ namespace Escape_Room
         /// Read-only dictionary mapping each sound type to its corresponding <see cref="SoundInterval"/>.
         /// Each <see cref="SoundInterval"/> contains one or more <see cref="Sound"/> objects defining frequency and duration.
         /// </summary>
-        public static readonly IReadOnlyDictionary<E_Sound, SoundInterval> soundCollection = new Dictionary<E_Sound, SoundInterval>()
+        public static readonly IReadOnlyDictionary<E_Sound, SoundInterval> soundCollection =
+            new Dictionary<E_Sound, SoundInterval>()
         {
             {
                 E_Sound.Movement,
                 new SoundInterval()
                 {
-                    sounds =
-                    [
+                    sounds = new Sound[]
+                    {
                         new Sound() { frequency = 523, duration = 80 }
-                    ]
+                    }
                 }
             },
             {
                 E_Sound.WallCollision,
                 new SoundInterval()
                 {
-                    sounds =
-                    [
+                    sounds = new Sound[]
+                    {
                         new Sound() { frequency = 330, duration = 80 },
                         new Sound() { frequency = 294, duration = 80 }
-                    ]
+                    }
                 }
             },
             {
                 E_Sound.KeyPickup,
                 new SoundInterval()
                 {
-                    sounds =
-                    [
+                    sounds = new Sound[]
+                    {
                         new Sound() { frequency = 392, duration = 100 },
                         new Sound() { frequency = 523, duration = 100 },
                         new Sound() { frequency = 659, duration = 100 }
-                    ]
+                    }
                 }
             },
             {
                 E_Sound.LevelFinished,
                 new SoundInterval()
                 {
-                    sounds =
-                    [
+                    sounds = new Sound[]
+                    {
                         new Sound { frequency = 196, duration = 100 },
-                        new Sound { frequency = 440, duration = 80 }, 
+                        new Sound { frequency = 440, duration = 80 },
                         new Sound { frequency = 220, duration = 100 },
-                        new Sound { frequency = 415, duration = 80 }, 
+                        new Sound { frequency = 415, duration = 80 },
                         new Sound { frequency = 196, duration = 100 },
-                        new Sound { frequency = 392, duration = 80 }, 
-                        new Sound { frequency = 196, duration = 120 } 
-                    ]
+                        new Sound { frequency = 392, duration = 80 },
+                        new Sound { frequency = 196, duration = 120 }
+                    }
                 }
             },
             {
                 E_Sound.GameFinished,
                 new SoundInterval()
                 {
-                    sounds =
-                    [
-                        new Sound { frequency = 523, duration = 250 }, 
-                        new Sound { frequency = 659, duration = 250 }, 
-                        new Sound { frequency = 784, duration = 250 }, 
+                    sounds = new Sound[]
+                    {
+                        new Sound { frequency = 523, duration = 250 },
+                        new Sound { frequency = 659, duration = 250 },
+                        new Sound { frequency = 784, duration = 250 },
                         new Sound { frequency = 1047, duration = 250 },
-                        new Sound { frequency = 784, duration = 250 }, 
-                        new Sound { frequency = 659, duration = 250 }, 
-                        new Sound { frequency = 523, duration = 250 }  
-                    ]
+                        new Sound { frequency = 784, duration = 250 },
+                        new Sound { frequency = 659, duration = 250 },
+                        new Sound { frequency = 523, duration = 250 }
+                    }
                 }
             },
             {
                 E_Sound.InvalidInput,
                 new SoundInterval()
                 {
-                    sounds = [
+                    sounds = new Sound[]
+                    {
                         new Sound { frequency = 164, duration = 60 },
                         new Sound { frequency = 147, duration = 60 }
-                    ]
+                    }
                 }
             },
             {
                 E_Sound.ValidInput,
                 new SoundInterval()
                 {
-                    sounds = [
+                    sounds = new Sound[]
+                    {
                         new Sound { frequency = 220, duration = 50 },
-                        new Sound { frequency = 261, duration = 50 } 
-                    ]
+                        new Sound { frequency = 261, duration = 50 }
+                    }
                 }
             },
             {
                 E_Sound.EndScreen,
                 new SoundInterval()
                 {
-                    sounds = [
+                    sounds = new Sound[]
+                    {
                         new Sound { frequency = 100, duration = 50 },
                         new Sound { frequency = 500, duration = 50 },
                         new Sound { frequency = 100, duration = 50 },
                         new Sound { frequency = 500, duration = 50 }
-                    ]
+                    }
                 }
             }
         };
